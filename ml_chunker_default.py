524d17c478b844f8a909cd521bb9233a21f52deb
Joel Klint <joel.klint@gmail.com>
2017-09-15 10:50:09 +0200

    finished lab

--- ml_chunker_default.py
+++ ml_chunker_default.py
@@ -66,16 +66,16 @@ def extract_features_sent(sentence, w_size, feature_names):
         # x is a row of X
         x = list()
         # The words in lower case
-        # for j in range(2 * w_size + 1):
-        #     x.append(padded_sentence[i + j][0].lower())
+        for j in range(2 * w_size + 1):
+            x.append(padded_sentence[i + j][0].lower())
         # The POS
         for j in range(2 * w_size + 1):
             x.append(padded_sentence[i + j][1])
         # The chunks (Up to the word)
-        """
+        
         for j in range(w_size):
-            feature_line.append(padded_sentence[i + j][2])
-        """
+            x.append(padded_sentence[i + j][2])
+        
         # We represent the feature vector as a dictionary
         X.append(dict(zip(feature_names, x)))
         # The classes are stored in a list
@@ -146,9 +146,9 @@ if __name__ == '__main__':
     train_corpus = 'data/train.txt'
     test_corpus = 'data/test.txt'
     w_size = 2  # The size of the context window to the left and right of the word
-    # feature_names = ['word_n2', 'word_n1', 'word', 'word_p1', 'word_p2',
-    #                  'pos_n2', 'pos_n1', 'pos', 'pos_p1', 'pos_p2']
-    feature_names = ['pos_n2', 'pos_n1', 'pos', 'pos_p1', 'pos_p2']
+    feature_names = ['word_n2', 'word_n1', 'word', 'word_p1', 'word_p2',
+                     'pos_n2', 'pos_n1', 'pos', 'pos_p1', 'pos_p2',
+                     'chunk_n2', 'chunk_n1']
 
     train_sentences = conll_reader.read_sentences(train_corpus)
 
